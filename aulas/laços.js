7b5231f5e4053e0433e65c2abada00421e964236
Thalyta <ssousathalyta@gmail.com>
2025-08-29 21:02:10 -0300

    fix: aula sobre laços e condicionais

--- aulas/laços.js
+++ aulas/laços.js
@@ -5,36 +5,6 @@ while (contador <= 5) {
     contador++;
 }
 
-let diasemana = 1;
-let nomedia;
-
-switch (diasemana) {
-    case 1:
-        nomedia = "Domingo";
-        break;
-    case 2:
-        nomedia = "Segunda-feira";
-        break;
-    case 3:
-        nomedia = "Terça-feira";
-        break;  
-    case 4:
-        nomedia = "Quarta-feira";
-        break;
-    case 5:
-        nomedia = "Quinta-feira";
-        break;
-    case 6:
-        nomedia = "Sexta-feira";
-        break;
-    case 7:
-        nomedia = "Sábado";
-        break;
-    default:
-        nomedia = "Dia inválido";
-}
-console.log(nomedia);
-
 let contador2 = 1;
 do {
     console.log(`Contador2: ${contador2}`);
